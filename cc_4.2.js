b023077c21f76d41008ee016c5c15acc612fbe89
Emily Steinmetz <emilysteinmetz@usf.edu>
2026-06-05 18:32:41 -0400

    Delete cc_4.2.js

--- cc_4.2.js
+++
@@ -1,7 +0,0 @@
-const products = [
-    { name: "Television", category: "electronics", price: 500, inventory: 12 },
-    { name: "Jeans", category: "apparel", price: 30, inventory: 47 },
-    { name: "Bread", category: "groceries", price: 3, inventory: 33 },
-    { name: "Trash Bag", category: "household", price: 10, inventory: 42 },
-    { name: "Pencil Case", category: "school", price: 8, inventory: 16 }
-];
